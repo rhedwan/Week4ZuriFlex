66ee0be692d72d525e70d9c5d43849cfc80f2ef7
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-06-16 20:46:38 +0100

    fix the email checker and the scoial icons hover effect

--- Registration/script.js
+++ Registration/script.js
@@ -7,7 +7,19 @@ const email = id("email"),
   errorMsg = classes("error"),
   failureIcon = classes("failure-icon");
 
-console.log(email, password, firstName, lastName, errorMsg);
+const validateEmail = (email) => {
+  return Boolean(
+    String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+  );
+};
+// console.log(validateEmail("afaf"));
+// console.log(validateEmail("afaf@daa.com"));
+// console.log(validateEmail("afaf @daa.com"));
+// console.log(validateEmail("afaf @daa/com"));
 
 let engine = (id, serial, message) => {
   if (id.value.trim() === "") {
@@ -20,11 +32,27 @@ let engine = (id, serial, message) => {
     failureIcon[serial].style.opacity = "0";
   }
 };
-// id("form");
+
+let emailChecker = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message[0];
+    id.style.border = "2px solid red";
+    failureIcon[serial].style.opacity = "1";
+    return;
+  } else if (!validateEmail(id.value)) {
+    errorMsg[serial].innerHTML = message[1];
+    id.style.border = "2px solid red";
+    failureIcon[serial].style.opacity = "1";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid #eee";
+    failureIcon[serial].style.opacity = "0";
+  }
+};
 document.querySelector(".btn").addEventListener("click", function (e) {
   e.preventDefault();
   engine(firstName, 0, "First Name cannot be blank");
   engine(lastName, 1, "Last Name cannot be blank");
-  engine(email, 2, "Email cannot be blank");
+  emailChecker(email, 2, ["Email cannot be blank", "Email is not valid"]);
   engine(password, 3, "Password cannot be blank");
 });
